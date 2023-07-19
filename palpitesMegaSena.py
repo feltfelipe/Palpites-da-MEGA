5d7e12eb6e80e51d9049212c32084f02f6dcd412
Luis Miranda <131805357+feltfelipe@users.noreply.github.com>
2023-07-18 23:20:16 -0300

    palpitesMegaSena.py

--- palpitesMegaSena.py
+++ palpitesMegaSena.py
@@ -1,5 +1,5 @@
-'''Faça um programa que ajude um jogador da MEGA SENA a criar palpites.
-O programa vai perguntar quantos jogos serão gerados e vai sortear 6 números entre 1 e 60 para cada jogo,
+'''
+O programa vai perguntar quantos jogos serão gerados e vai sortear 6 números entre 1 e 60 para cada jogo (editável),
 cadastrando tudo em uma lista composta.'''
 from random import randint
 temp = []
